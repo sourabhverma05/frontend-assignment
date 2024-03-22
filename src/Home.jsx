71caded0116d497e5e92aa54b99fcec5bb93e531
sourabh verma <sourabhverma3105@gmail.com>
2024-03-22 17:48:59 +0530

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
     }
 
     return (
-        <div className='w-screen 2xl:h-full 2xl:w-full 2xl:mx-auto 2xl:my-[5%] my-[100px]'>
+        <div className='w-screen 2xl:h-full 2xl:w-full 2xl:mx-auto 2xl:my-[5%]'>
             <TooltipProvider>
                 <div className='flex flex-col sm:flex-col 2xl:flex-row'>
                     <img
@@ -28,7 +28,7 @@ const HomePage = () => {
 
                         <div className={`flex sm:flex-col flex-col items-center w-[300px] md:w-[300px] 2xl:flex-row`}>
 
-                            <p className='py-3 px-1 sm:w-[110px] w-[110px] text-xs sm:text-xs bg-[#3552aa] hover:text-white tracking-wide text-center font-medium rounded-sm lg:w-[150px] lg:text-sm xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Research</p>
+                            <p className='py-3 px-1 sm:w-[110px] w-[110px] text-xs sm:text-xs bg-[#3552aa] hover:text-white tracking-wide text-center font-medium rounded-sm lg:w-[150px] lg:text-base xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Research</p>
                             <div className='flex items-center'>
                                 <img src={ThreeArrow} className='hidden sm:hidden w-[200px] h-[70px] 2xl:py-2 2xl:w-[300px] 2xl:h-20 2xl:block' alt="#" />
                                 <img src={Arrow} className='h-full sm:h-full 2xl:hidden' alt="#" />
@@ -36,7 +36,7 @@ const HomePage = () => {
                         </div>
 
                         <div className='flex flex-col sm:flex-col items-center w-[300px] 2xl:flex-row'>
-                            <p className='py-3 px-1 sm:w-[110px] w-[110px] sm:text-xs text-xs text-center bg-[#82B3E8] hover:text-white tracking-wide font-medium rounded-sm lg:w-[150px] lg:text-sm xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Planning</p>
+                            <p className='py-3 px-1 sm:w-[110px] w-[110px] sm:text-xs text-xs text-center bg-[#82B3E8] hover:text-white tracking-wide font-medium rounded-sm lg:w-[150px] lg:text-base xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Planning</p>
                             <div className='flex items-center'>
                                 <img src={ThreeArrow} className='hidden sm:hidden w-[200px] h-[70px] 2xl:block 2xl:py-2 2xl:w-[300px] 2xl:h-20' alt="#" />
                                 <img src={Arrow} className='sm:h-full h-full 2xl:hidden' alt="#" />
@@ -44,7 +44,7 @@ const HomePage = () => {
                         </div>
 
                         <div className='flex flex-col sm:flex-col items-center w-[300px] 2xl:flex-row'>
-                            <p className='py-3 px-1 sm:w-[110px] sm:text-xs w-[110px] text-xs bg-[#E96343] hover:text-white tracking-wide font-medium text-center rounded-sm lg:w-[150px] lg:text-sm  xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Desiging</p>
+                            <p className='py-3 px-1 sm:w-[110px] sm:text-xs w-[110px] text-xs bg-[#E96343] hover:text-white tracking-wide font-medium text-center rounded-sm lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Desiging</p>
                             <div className='flex items-center'>
                                 <img src={ThreeArrow} className='hidden sm:hidden w-[200px] h-[70px] 2xl:block 2xl:py-2 2xl:w-[300px] 2xl:h-20' alt="#" />
                                 <img src={Arrow} className='sm:h-full 2xl:hidden' alt="#" />
@@ -52,7 +52,7 @@ const HomePage = () => {
                         </div>
 
                         <div className='flex flex-col sm:flex-col items-center w-[300px] 2xl:flex-row'>
-                            <p className='py-3 px-1 sm:w-[110px] sm:text-xs w-[110px] text-xs bg-[#E8919D] hover:text-white text-center tracking-wide font-medium rounded-sm lg:w-[150px] lg:text-sm  xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Manufacturing</p>
+                            <p className='py-3 px-1 sm:w-[110px] sm:text-xs w-[110px] text-xs bg-[#E8919D] hover:text-white text-center tracking-wide font-medium rounded-sm lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Manufacturing</p>
                             <div className='flex items-center'>
                                 <img src={ThreeArrow} className='hidden sm:hidden w-[200px] h-[70px] 2xl:block 2xl:py-2 2xl:w-[300px] 2xl:h-20' alt="#" />
                                 <img src={Arrow} className='sm:h-full 2xl:hidden' alt="#" />
@@ -60,7 +60,7 @@ const HomePage = () => {
                         </div>
 
                         <div className='flex flex-col sm:flex-col items-center w-[300px] 2xl:flex-row   '>
-                            <p className='py-3 px-1 sm:w-[110px] sm:text-xs w-[110px] text-xs bg-[#a684eb] hover:text-white tracking-wide font-medium text-center rounded-sm lg:w-[150px] lg:text-sm  xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Salse/Marketing</p>
+                            <p className='py-3 px-1 sm:w-[110px] sm:text-xs w-[110px] text-xs bg-[#a684eb] hover:text-white tracking-wide font-medium text-center rounded-sm lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-3 2xl:w-[200px]'>Salse/Marketing</p>
                             <div className='flex items-center'>
                                 <img src={ThreeArrow} className='hidden sm:hidden w-[200px] h-[70px] 2xl:block 2xl:py-2 2xl:w-[300px] 2xl:h-20' alt="#" />
                                 <img src={Arrow} className='sm:h-full 2xl:hidden' alt="#" />
@@ -68,12 +68,12 @@ const HomePage = () => {
                         </div>
                     </div>
 
-                    <div className='flex flex-row gap-[5px] m-auto sm:flex-row sm:gap-[20px] sm:justify-center md:gap-[45px] lg:gap-[65px] xl:gap-[60px] xl:ms-[215px] 2xl:gap-7 2xl:ms-0 2xl:flex-col 2xl:justify-center 2xl:my-auto'>
+                    <div className='flex flex-row gap-[5px] m-auto sm:flex-row sm:gap-[20px] sm:justify-center md:gap-[45px] lg:gap-[65px] xl:gap-[60px] xl:ms-[215px] 2xl:gap-7 2xl:ms-[0px] 2xl:flex-col 2xl:justify-center 2xl:my-auto'>
 
                         <div className="flex flex-col gap-4 sm:flex sm:flex-col sm:gap-3 2xl:gap-[12px]">
                             <div className='flex sm:flex items-center relative '>
                                 <Button onClick={() => handleBtnsDetail("externalBtn")}
-                                    className='bg-[#3552aa] hover:bg-[#4768cd] hover:text-white text-black focus:text-white w-[110px] tracking-wide py-3 px-1 text-sm font-medium rounded-sm sm:w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                    className='bg-[#3552aa] hover:bg-[#4768cd] hover:text-white text-black focus:text-white w-[110px] tracking-wide py-3 px-1 text-sm font-medium rounded-sm sm:w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'>
                                     External
                                 </Button>
                                 {hide === "externalBtn" && <div className={`absolute sm:top-[30px] sm:left-[40px] top-[30px] left-[40px] xl:ms-[12px] 2xl:absolute 2xl:right-[-20rem] 2xl:top-[-1.5rem] 2xl:ms-[85px] 2xl:flex`}>
@@ -90,7 +90,7 @@ const HomePage = () => {
 
                             <div className='flex items-center relative'>
                                 <Button onClick={() => handleBtnsDetail("internalBtn")}
-                                    className='text-black sm:w-[110px] focus:text-white bg-[#3552aa] hover:bg-[#4768cd] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'
+                                    className='text-black sm:w-[110px] focus:text-white bg-[#3552aa] hover:bg-[#4768cd] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'
                                 >
                                     Internal
                                 </Button>
@@ -110,7 +110,7 @@ const HomePage = () => {
                             <div className="relative">
                                 <div className='flex items-center'>
                                     <Button onClick={() => handleBtnsDetail("PRDBtn")}
-                                        className='text-black sm:w-[110px] focus:text-white bg-[#82B3E8] hover:bg-[#91c1f6] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                        className='text-black sm:w-[110px] focus:text-white bg-[#82B3E8] hover:bg-[#91c1f6] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'>
                                         PRD
                                     </Button>
                                     {hide === "PRDBtn" && <div className={`absolute sm:top-[35px] sm:left-[50px] top-[30px] left-[40px] xl:ms-[27px] 2xl:ms-[75px] 2xl:top-[-1.5rem] 2xl:flex 2xl:right-[-20.4rem]`}>
@@ -128,7 +128,7 @@ const HomePage = () => {
 
                             <div className='flex items-center relative'>
                                 <Button onClick={() => handleBtnsDetail("specsBtn")}
-                                    className='text-black sm:w-[110px] focus:text-white bg-[#82B3E8] hover:bg-[#91c1f6]  hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                    className='text-black sm:w-[110px] focus:text-white bg-[#82B3E8] hover:bg-[#91c1f6]  hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base xl:w-[130px] xl:text-sm 2xl:py-2'>
                                     Specs
                                 </Button>
                                 {hide === "specsBtn" && <div className={`absolute sm:top-[35px] sm:left-[50px] top-[30px] left-[40px] xl:ms-[27px] 2xl:ms-[75px] 2xl:top-[-1.5rem] 2xl:flex 2xl:right-[-20.4rem]`}>
@@ -146,7 +146,7 @@ const HomePage = () => {
                         <div className="flex flex-col gap-4 sm:flex sm:flex-col sm:gap-3 2xl:gap-[12px]">
                             <div className='flex items-center relative'>
                                 <Button onClick={() => handleBtnsDetail("hardwareBtn")}
-                                    className='text-black sm:w-[110px] focus:text-white bg-[#E96343] hover:bg-[#f3704f] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                    className='text-black sm:w-[110px] focus:text-white bg-[#E96343] hover:bg-[#f3704f] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'>
                                     Hardware
                                 </Button>
                                 {hide === "hardwareBtn" && <div className={`absolute sm:top-[35px] sm:left-[50px]  top-[30px] left-[40px] xl:ms-[29px] 2xl:ms-[75px] 2xl:top-[-1.5rem] 2xl:flex 2xl:right-[-20.4rem]`}>
@@ -163,7 +163,7 @@ const HomePage = () => {
 
                             <div className='flex items-center relative'>
                                 <Button onClick={() => handleBtnsDetail("softwareBtn")}
-                                    className='text-black sm:w-[110px] focus:text-white bg-[#E96343] hover:bg-[#f3704f] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                    className='text-black sm:w-[110px] focus:text-white bg-[#E96343] hover:bg-[#f3704f] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'>
                                     Software
                                 </Button>
                                 {hide === "softwareBtn" && <div className={`absolute sm:top-[35px] sm:left-[50px] top-[30px] left-[40px] xl:ms-[29px] 2xl:ms-[75px] 2xl:top-[-1.5rem] 2xl:flex 2xl:right-[-20.4rem]`}>
@@ -181,7 +181,7 @@ const HomePage = () => {
                         <div className="flex flex-col gap-4 sm:flex sm:flex-col sm:gap-3 2xl:gap-[12px]">
                             <div className='flex items-center relative'>
                                 <Button onClick={() => handleBtnsDetail("materialBtn")}
-                                    className='text-black sm:w-[110px] focus:text-white bg-[#E8919D] hover:bg-[#eb9aa4] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                    className='text-black sm:w-[110px] focus:text-white bg-[#E8919D] hover:bg-[#eb9aa4] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'>
                                     Material
                                 </Button>
                                 {hide === "materialBtn" && <div className={`absolute sm:top-[35px] sm:left-[50px]  top-[30px] left-[40px] xl:ms-[28px] 2xl:ms-[75px] 2xl:top-[-1.5rem] 2xl:flex 2xl:right-[-20.4rem]`}>
@@ -198,7 +198,7 @@ const HomePage = () => {
 
                             <div className='flex items-center relative'>
                                 <Button onClick={() => handleBtnsDetail("productionBtn")}
-                                    className='text-black sm:w-[110px] focus:text-white bg-[#E8919D] hover:bg-[#eb9aa4] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                    className='text-black sm:w-[110px] focus:text-white bg-[#E8919D] hover:bg-[#eb9aa4] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'>
                                     Production
                                 </Button>
                                 {hide === "productionBtn" && <div className={`absolute sm:top-[35px] sm:left-[50px] top-[30px] left-[40px] xl:ms-[28px] 2xl:ms-[75px] 2xl:top-[-1.5rem] 2xl:flex 2xl:right-[-20.4rem]`}>
@@ -216,7 +216,7 @@ const HomePage = () => {
                         <div className="flex flex-col gap-4 sm:flex sm:flex-col sm:gap-3 2xl:gap-[12px]">
                             <div className='flex items-center relative'>
                                 <Button onClick={() => handleBtnsDetail("onlineBtn")}
-                                    className='text-black sm:w-[110px] focus:text-white bg-[#A684EB] hover:bg-[#ae8eed] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                    className='text-black sm:w-[110px] focus:text-white bg-[#A684EB] hover:bg-[#ae8eed] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'>
                                     Online
                                 </Button>
                                 {hide === "onlineBtn" && <div className={`absolute sm:top-[35px] sm:left-[50px] top-[30px] left-[40px] xl:ms-[28px] 2xl:ms-[75px] 2xl:top-[-1.5rem] 2xl:flex 2xl:right-[-20.4rem]`}>
@@ -233,7 +233,7 @@ const HomePage = () => {
 
                             <div className='flex items-center relative'>
                                 <Button onClick={() => handleBtnsDetail("dealearshipBtn")}
-                                    className='text-black sm:w-[110px] focus:text-white bg-[#A684EB] hover:bg-[#ae8eed] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-lg  xl:w-[130px] xl:text-sm 2xl:py-2'>
+                                    className='text-black sm:w-[110px] focus:text-white bg-[#A684EB] hover:bg-[#ae8eed] hover:text-white tracking-wide py-3 px-1 text-sm font-medium rounded-sm w-[110px] lg:w-[150px] lg:text-base  xl:w-[130px] xl:text-sm 2xl:py-2'>
                                     Dealearship
                                 </Button>
                                 {hide === "dealearshipBtn" && <div className={`absolute sm:top-[35px] sm:left-[50px] top-[30px] left-[40px] xl:ms-[28px] 2xl:ms-[75px] 2xl:top-[-1.5rem] 2xl:flex 2xl:right-[-20.4rem]`}>
--- src/components/bc-items.jsx
+++ src/components/bc-items.jsx
@@ -10,14 +10,14 @@ const B2CItems = () => {
         <>
             <DropdownMenu>
                 <DropdownMenuTrigger>
-                    <p className={`relative sm:relative w-[110px] sm:w-[110px] bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm lg:w-[150px] lg:text-lg xl:w-[130px] xl:text-sm 2xl:py-2 2xl:w-[130px]`}>B2C</p>
+                    <p className={`relative sm:relative w-[110px] sm:w-[110px] bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm lg:w-[150px] lg:text-base xl:w-[130px] xl:text-sm 2xl:py-2 2xl:w-[130px]`}>B2C</p>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className={`flex flex-col`}>
                     <DropdownMenuSeparator />
                     <Tooltip>
                         <TooltipTrigger>
                             <DropdownMenuItem>
-                                <p className='w-[110px] sm:w-[110px] bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm lg:w-[130px] lg:text-lg xl:w-[130px] xl:text-sm 2xl:py-2 2xl:w-[130px]'>Online</p>
+                                <p className='w-[110px] sm:w-[110px] bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm lg:w-[130px] lg:text-base xl:w-[130px] xl:text-sm 2xl:py-2 2xl:w-[130px]'>Online</p>
                             </DropdownMenuItem>
                         </TooltipTrigger>
                         <TooltipContent>
